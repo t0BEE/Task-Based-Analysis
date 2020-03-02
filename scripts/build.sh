17b2337cbc9322c496fd7d05d4ae9ade8c7419fe
t0BEE <tobiasschiffm@web.de>
2020-03-02 09:39:47 +0100

    Add sequential project

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# build openMP generic algorithm
+
+# create folder for all build solutions
+
 mkdir -p ../build_output
 cd ../build_output
+
+# build sequential program
+cmake ../sequential
+cmake --build .
+
+# build openMP generic algorithm
 cmake ../openMP
 cmake --build .
