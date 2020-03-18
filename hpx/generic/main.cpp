6ab6a9c49290f3e3b0b3e97d9ff9eb0c0edf47f1
t0BEE <tobiasschiffm@web.de>
2020-03-18 12:51:33 +0100

    Init generic hpx algorithm

--- hpx/generic/main.cpp
+++ hpx/generic/main.cpp
@@ -2,6 +2,11 @@
 #include <cstdlib>
 #include <cmath>
 #include <chrono>
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/iostreams.hpp>
+#include <hpx/include/async.hpp>
+#include <hpx/parallel/algorithms/for_loop.hpp>
+#include <hpx/parallel/execution_policy.hpp>
 
 #define VECTOR_SIZE 10000
 
@@ -11,33 +16,30 @@ float vectors[2][VECTOR_SIZE];
 void sum_vector(int vectorIndex, int depth = 1){
     int stepSize = (int) pow( 2, depth);
     if (stepSize > VECTOR_SIZE) return;
-    for (int i = 0; i < VECTOR_SIZE; i += stepSize) {
-        // the amount of tasks it not always dividable by 2,
-        // the result of the last task has to be added to the previous result
-        // this is the case if the modulo calculation of the vector size is greater or equal to the half of the step size
-        if (i > VECTOR_SIZE - stepSize) {
-            if (VECTOR_SIZE % stepSize >= stepSize / 2) {
-                vectors[vectorIndex][i - stepSize] += vectors[vectorIndex][i];
+    hpx::future<void> loop = hpx::parallel::v2::for_loop_strided(
+	hpx::parallel::execution::parallel_task_policy(),
+	0, VECTOR_SIZE, stepSize, [&](int i)
+	{
+            // the amount of tasks it not always dividable by 2,
+            // the result of the last task has to be added to the previous result
+            // this is the case if the modulo calculation of the vector size is greater or equal to the half of the step size
+            if (i > VECTOR_SIZE - stepSize) {
+            	if (VECTOR_SIZE % stepSize >= stepSize / 2) {
+             	    vectors[vectorIndex][i - stepSize] += vectors[vectorIndex][i];
+            	}
+            } else {
+                vectors[vectorIndex][i] += vectors[vectorIndex][i + (stepSize / 2)];
             }
-        } else {
-            vectors[vectorIndex][i] += vectors[vectorIndex][i + (stepSize / 2)];
-        }
-    }
+	});
+    loop.get();
     sum_vector(vectorIndex, depth + 1);
 }
 
 
-int main(int argc, char *argv[]) {
+int hpx_main(hpx::program_options::variables_map& vm) {
 
-    // parse parameters
-    // first parameter: how often calculate from one vector to another
-    // second parameter: task size (how many tasks per vector, VECTOR_SIZE/task size)
-    if (argc != 3){
-        std::cout << "Some parameters are missing!" << std::endl;
-        return -1;
-    }
-    int turns = atoi(argv[1]);
-    int taskSize = atoi(argv[2]);
+    int turns = vm["turns"].as<int>();
+    int taskSize =vm ["taskSize"].as<int>();
 
     // seed the random number generator with a constant to create a deterministic generation
     srand(42);
@@ -69,10 +71,30 @@ int main(int argc, char *argv[]) {
 
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << "Execution time: " << duration.count() << std::endl;
+    std::cout << "Execution time: " << duration.count() << "\n" << hpx::flush;
 
     // Output the solution for debugging
-    std::cout << vectors[0][0] << std::endl;
+    hpx::cout << vectors[0][0] << "\n" << hpx::flush;
 
-    return 0;
+    return hpx::finalize();
+}
+
+int main(int argc, char *argv[]){
+    // parse parameters
+    // Configure application-specific options
+    hpx::program_options::options_description
+       desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
+
+    desc_commandline.add_options()
+        ( "turns",
+          hpx::program_options::value<int>()->default_value(10),
+          "specifies how often vectors are copied");
+
+    desc_commandline.add_options()
+        ( "taskSize",
+          hpx::program_options::value<int>()->default_value(10),
+          "specifies how many vector elements are used by a task");
+
+    // Init and run HPX runtime environment
+    return hpx::init(desc_commandline, argc, argv);
 }
