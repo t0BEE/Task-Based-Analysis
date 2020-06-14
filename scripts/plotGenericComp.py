372fdb6ea360f70a557e66ce0a64481259f8aec9
t0BEE <tobiasschiffm@web.de>
2020-06-14 10:59:20 +0200

    Add Scatterplot to compare generic algorithms

--- scripts/plotGenericComp.py
+++ scripts/plotGenericComp.py
@@ -6,6 +6,8 @@ import numpy as np
 path_to_data = 'metrics/'
 data_file = [pos for pos in os.listdir(path_to_data) if pos.endswith('.data')]
 
+taskSizeList = [1, 4, 10, 20, 100, 200]
+
 def mean(in_list):
     length = len(in_list)
     tmp_sum = 0
@@ -13,16 +15,40 @@ def mean(in_list):
         tmp_sum += in_list[position]
     return tmp_sum / length
 
+
 genLists = {
-    "seq": [],
-    "omp": [],
-    "hpx": []
+    "seq": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    "omp": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    "hpx": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 }
 
+nrFiles = 0
 for index, js in enumerate(data_file):
     with open(os.path.join(path_to_data, js), 'r') as j_file:
+        nrFiles += 1
         temp_dict = json.load(j_file)
-        genLists["seq"].extend(temp_dict['sequential'])
-        genLists["omp"].extend(temp_dict['openMP'])
-        genLists["hpx"].extend(temp_dict['hpx'])
+        for i in range(len(genLists['seq'])):
+            genLists['seq'][i] += temp_dict['sequential'][i]
+            genLists['omp'][i] += temp_dict['openMP'][i]
+            genLists['hpx'][i] += temp_dict['hpx'][i]
 
+
+for i in range(len(genLists['seq'])):
+    genLists['seq'][i] = genLists['seq'][i] / nrFiles
+    genLists['omp'][i] = genLists['omp'][i] / nrFiles
+    genLists['hpx'][i] = genLists['hpx'][i] / nrFiles
+
+
+#plt.scatter(taskSizeList, genLists['seq'])
+fig = plt.figure()
+#ax = fig.add_axes([0, 0, 1, 1])
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(taskSizeList, genLists['seq'], color='red', label='Sequential')
+ax.scatter(taskSizeList, genLists['omp'], color='blue', label='OpenMP')
+ax.scatter(taskSizeList, genLists['hpx'], color='green', label='HPX')
+ax.set_xlabel('Task Size Parameter')
+ax.set_ylabel('Execution Time')
+ax.set_title('Generic Comparison')
+
+plt.legend(loc=1)
+plt.show()
--- scripts/spack_pipline.sh
+++ scripts/spack_pipline.sh
@@ -14,48 +14,5 @@ done
 pipenv run python3 ./plot.py
 
 
-# compare generic execution of sequential, OpenMP and HPX
-# For different task sizes
-
-for run in $(seq 1 20)
-do
-  echo "{" > genericComp_$run.data
-  echo -n -e "\"sequential\": [" >> genericComp_$run.data
-  for i in 1 4 10 20 100 200
-  do
-    ../build_output/sequentialGeneric 50 $i >> genericComp_$run.data
-    if [[ $i -ne 200 ]]; then
-      echo -n ", " >> genericComp_$run.data
-    else
-      echo "]," >> genericComp_$run.data
-    fi
-  done
-
-  echo -n -e "\"openMP\": [" >> genericComp_$run.data
-  for i in 1 4 10 20 100 200
-  do
-    ../build_output/openMPGeneric 50 $i >> genericComp_$run.data
-    if [[ $i -ne 200 ]]; then
-      echo -n ", " >> genericComp_$run.data
-    else
-      echo "]," >> genericComp_$run.data
-    fi
-  done
-
-  echo -n -e "\"hpx\": [" >> genericComp_$run.data
-  for i in 1 4 10 20 100 200
-  do
-    ../build_output/hpxGeneric 50 $i >> genericComp_$run.data
-    if [[ $i -ne 200 ]]; then
-      echo -n ", " >> genericComp_$run.data
-    else
-      echo "]" >> genericComp_$run.data
-    fi
-  done
-  echo -e "}" >> genericComp_$run.data
-done
-
-mv *.data metrics/
-
-pipenv run python3 ./plotGenericComp.py
+./.genericComp
 
